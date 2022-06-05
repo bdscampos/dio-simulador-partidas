7ebc327dd94faf2201f0552808072121f252636e
Bruno Campos <bdscampos@gmail.com>
2022-06-05 16:46:18 -0300

    Finished layout definition and details screen

---
+++ app/src/main/java/com/bdscampos/simulator/DetailActivity.kt
@@ -0,0 +1,18 @@
+package com.bdscampos.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bdscampos.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
