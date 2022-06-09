7f5bad9615cdff6ca36c89a0f8336675ec38c931
Bruno Campos <bdscampos@gmail.com>
2022-06-08 22:22:22 -0300

    Added Firebase support, Analytics and Crashlytics

--- app/src/main/java/com/bdscampos/simulator/ui/MainActivity.java
+++ app/src/main/java/com/bdscampos/simulator/ui/MainActivity.java
@@ -3,12 +3,10 @@ package com.bdscampos.simulator.ui;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.bdscampos.simulator.R;
 import com.bdscampos.simulator.data.MatchesApi;
@@ -16,8 +14,8 @@ import com.bdscampos.simulator.databinding.ActivityMainBinding;
 import com.bdscampos.simulator.domain.Match;
 import com.bdscampos.simulator.ui.adapter.MatchesAdapter;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.gson.Gson;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -30,7 +28,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private MatchesApi matchesApi;
-    private MatchesAdapter matchesAdapter;
+    private MatchesAdapter matchesAdapter = new MatchesAdapter(Collections.emptyList());
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
     private void setupMatchesList() {
         binding.rvMatches.setHasFixedSize(true);
         binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvMatches.setAdapter(matchesAdapter);
         findMatchesFromApi();
     }
 
@@ -71,13 +70,14 @@ public class MainActivity extends AppCompatActivity {
                 public void onAnimationEnd(Animator animation) {
                     Random random = new Random();
                     for (int i = 0; i < matchesAdapter.getItemCount(); i++){
-                        Match match =matchesAdapter.getMatches().get(i);
+                        Match match = matchesAdapter.getMatches().get(i);
                         match.getHomeTeam().setScore(random.nextInt(match.getHomeTeam().getStars() + 1));
                         match.getAwayTeam().setScore(random.nextInt(match.getAwayTeam().getStars() + 1));
                         matchesAdapter.notifyItemChanged(i);
                     }
                 }
             });
+            //throw new RuntimeException("Teste Crash");
         });
     }
 
